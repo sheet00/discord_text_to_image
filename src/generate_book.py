1871cf934e9b37257dbcf993c4f6d99189bb3fd3
sheet00 <10631639+sheet00@users.noreply.github.com>
2025-04-26 14:42:41 +0900

    md to json修正

--- src/generate_book.py
+++ src/generate_book.py
@@ -1,6 +1,9 @@
 from icecream import ic
 import json
 import markdown
+import bs4
+import json
+import re
 
 
 def markdown_to_json(markdown_file):
@@ -11,7 +14,16 @@ def markdown_to_json(markdown_file):
         markdown_text = f.read()
 
     html = markdown.markdown(markdown_text)
-    return {"html": html}
+    soup = bs4.BeautifulSoup(html, "html.parser")
+
+    title = soup.find("h2").text
+
+    paragraphs = []
+    for p in soup.find_all("p"):
+        text = p.text.replace("\n", "").replace(" ", "")
+        paragraphs.append({"p": text})
+
+    return json.dumps({"title": title, "paragraph": paragraphs}, ensure_ascii=False)
 
 
 def main():
