aabc95c9e6417984df8bdeb7b6d076505c609d36
sheet00 <10631639+sheet00@users.noreply.github.com>
2025-04-27 15:30:50 +0900

    voice リトライ処理追加

--- src/generate_voice.py
+++ src/generate_voice.py
@@ -1,8 +1,10 @@
 import os
+from retry import retry
 import datetime
 import requests
 
 SPEED_SCALE = 1.2
+
 
 def check_voicevox_server():
     """
@@ -10,11 +12,13 @@ def check_voicevox_server():
     接続できない場合は0を返す
     """
     try:
-        response = requests.get('http://127.0.0.1:50021/version')
+        response = requests.get("http://127.0.0.1:50021/version")
         return response.status_code
     except requests.exceptions.RequestException:
         return 0
 
+
+@retry(tries=10)
 def synthesize_voice_with_timestamp(text, speaker=1):
     print(f"音声合成するテキスト: {text}")
 
@@ -32,8 +36,10 @@ def synthesize_voice_with_timestamp(text, speaker=1):
 
     # 1. テキストから音声合成のためのクエリを作成
     print("音声合成クエリを作成中...")
-    query_payload = {'text': text, 'speaker': speaker}
-    query_response = requests.post('http://localhost:50021/audio_query', params=query_payload)
+    query_payload = {"text": text, "speaker": speaker}
+    query_response = requests.post(
+        "http://localhost:50021/audio_query", params=query_payload
+    )
 
     if query_response.status_code != 200:
         print(f"Error in audio_query: {query_response.text}")
@@ -41,18 +47,20 @@ def synthesize_voice_with_timestamp(text, speaker=1):
 
     print("クエリ作成完了")
     query = query_response.json()
-    query['speedScale'] = SPEED_SCALE
+    query["speedScale"] = SPEED_SCALE
 
     # 2. クエリを元に音声データを生成
     print("音声データを生成中...")
-    synthesis_payload = {'speaker': speaker}
+    synthesis_payload = {"speaker": speaker}
     # print(query)
-    synthesis_response = requests.post('http://localhost:50021/synthesis', params=synthesis_payload, json=query)
+    synthesis_response = requests.post(
+        "http://localhost:50021/synthesis", params=synthesis_payload, json=query
+    )
 
     if synthesis_response.status_code == 200:
         # 音声ファイルとして保存
         print("音声ファイルを保存中...")
-        with open(filepath, 'wb') as f:
+        with open(filepath, "wb") as f:
             f.write(synthesis_response.content)
         print(f"音声が {filepath} に保存されました。")
         return filepath
